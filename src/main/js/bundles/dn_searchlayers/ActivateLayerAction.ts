37860c986cbf63216ec81669ebebecd4a4af5244
Lia Kirsch <l.kirsch@conterra.de>
2023-06-28 10:17:52 +0200

    add types, update imports

---
+++ src/main/js/bundles/dn_searchlayers/ActivateLayerAction.ts
@@ -0,0 +1,135 @@
+///
+/// Copyright (C) 2023 con terra GmbH (info@conterra.de)
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///         http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+
+import async from "apprt-core/async";
+import { InjectedReference } from "apprt-core/InjectedReference";
+
+export default class ActivateLayerAction {
+    public id: string;
+    private _tocToggleTool: InjectedReference<any>;
+    private _properties: InjectedReference<Record<string, any>>;
+    private _tocWidget: InjectedReference<any>;
+
+    constructor() {
+        this.id = "activatelayer";
+    }
+
+    trigger(options:any):void {
+        if (!options || !options.items) {
+            throw new Error(
+                "SearchLayerActivateAction.trigger: " +
+                "Cannot execute SearchLayerActivateAction since no trigger options with 'items'" +
+                "property have been specified."
+            );
+        }
+
+        if (!options.items.length) {
+            return;
+        }
+
+        if (options.source?.id !== "searchlayersstore") {
+            return;
+        }
+
+        // open toc
+        this._tocToggleTool.set("active", true);
+
+        const layer = options.items[0];
+        this.changePropsForEveryLayer(layer);
+        async(() => {
+            this.highlightTocEntry(layer);
+        }, 100);
+
+    }
+
+    private highlightTocEntry(layer:any) {
+        const tocEntryHighlightTime = this._properties.tocEntryHighlightTime;
+        // highlight layer entry in toc
+        const tocItemUid:string = this.buildUID(layer);
+        const cssValidId: string= tocItemUid.replace(/[^_a-zA-Z0-9-]/g, '_');
+        const domElementList: HTMLCollectionOf<Element>= document.getElementsByClassName("ct-toc__layer-tree-item--" + cssValidId);
+        const domElement: Element = domElementList.length ? domElementList[0] : undefined;
+        domElement?.classList.add("highlight");
+        // scroll to highlighted layer
+        async(() => {
+            domElement?.scrollIntoView();
+        }, 500);
+        // remove highlight from layer entry after a configurable time
+        async(() => {
+            domElement?.classList.remove("highlight");
+        }, tocEntryHighlightTime);
+    }
+
+    /**
+     * Function used to recursively make all parent layers of a layer visible
+     *
+     * @param layer Esri Layer which has to made visible, including all parents
+     */
+    private changePropsForEveryLayer(layer:any) {
+        // set visible property to true
+        layer.visible = true;
+
+        // get toc model item and set open to true
+        const tocModelItem = this.getTocModelItem(layer.uid);
+        if (tocModelItem) {
+            tocModelItem.open = true;
+        }
+
+        // if layer has parent call method again
+        if (layer.parent) {
+            this.changePropsForEveryLayer(layer.parent);
+        }
+    }
+
+    private getTocModelItem(uid:any) {
+        const tocWidget = this._tocWidget;
+        const vm = tocWidget.getVM();
+        const operationalRoot: any = vm.operationalRoot;
+
+        let tocItem: any;
+        operationalRoot.visitTree((it: { reference: { uid: any; }; })=> {
+            if (it.reference.uid === uid) {
+                tocItem = it;
+            }
+        });
+        return tocItem;
+    }
+
+    /**
+     * Method copied from TocItemsToMapSync file in mapapps.
+     *
+     * @param layerOrSublayer
+     * @returns {string|*}
+     * @private
+     */
+    private buildUID(layerOrSublayer: { id: string; layer: any; }) {
+        if (!layerOrSublayer) {
+            return;
+        }
+        const localId: string = layerOrSublayer.id;
+        if (!this.isSublayer(layerOrSublayer)) {
+            // assumed to be unique
+            return localId;
+        }
+        const uidOfSublayersRoot: string = this.buildUID(layerOrSublayer.layer);
+        return uidOfSublayersRoot + "$" + localId;
+    }
+
+    private isSublayer(layer: { hasOwnProperty: (arg0: string) => any; }) {
+        // eslint-disable-next-line no-prototype-builtins
+        return layer.hasOwnProperty("layer");
+    }
+}
